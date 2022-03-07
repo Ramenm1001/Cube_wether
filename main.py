51f8df1c5616bc1965933075780a65bc4c1ea6f9
Ramenm1001 <47206896+Ramenm1001@users.noreply.github.com>
2022-03-08 02:01:52 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask
 import os
-
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return """
-    OK
-    """
+    resp = {"температура": 15,
+            "освещенность": 150,
+            "ну и еще немного": 200}
+    return json.dumps(resp)
 
 
 if __name__ == '__main__':
