36170445bb6caaec3567d77c60c70d115dba59c5
Ramenm1001 <47206896+Ramenm1001@users.noreply.github.com>
2022-03-16 00:03:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from flask import Flask, request, jsonify
 import os
 import json
 from flask_cors import CORS
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+DATABASE_URL = os.environ['postgres://eejfkwjeffaiyc:2cfad4401829e07ddc16bcb439be7be2187ed7237469aec6c0037a06993b0558@ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/d6gqkisgrtebp3']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 some_data = "155"
 
