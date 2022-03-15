f29d6e727860cb6a6b0f7db3edf9b69f40673eec
Ramenm1001 <47206896+Ramenm1001@users.noreply.github.com>
2022-03-16 01:15:59 +1000

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,8 @@ app = Flask(__name__)
 CORS(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-DATABASE_URL = os.environ['postgres://eejfkwjeffaiyc:2cfad4401829e07ddc16bcb439be7be2187ed7237469aec6c0037a06993b0558@ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/d6gqkisgrtebp3']
+DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-if conn:
-    some_data = "155"
-else:
-    some_data = "55"
 
 @app.route('/')
 def main(methods=['GET']):
